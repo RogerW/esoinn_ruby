6e3ff442dc3838b9d568a010afe3eb9180849be0
unknown <StSVorobev@SHROB-H80003.bee.vimpelcom.ru>
2015-09-17 17:23:34 +0500

    start use RGL

--- esoinn.rb
+++ esoinn.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
+require 'rgl/adjacency'
 
 class ESOINN
   def initialize(dimensions, max_age, iter_threshold, c1, c2)
-    @vertex = []
-    @edges = {}
+    @graph = RGL::AdjacencyGraph[]
+    @age_edges = {}
 
     @dimensions = dimensions
     @max_age = max_age
@@ -23,43 +24,57 @@ class ESOINN
   private
 
   def add_signal(input_signal)
-    if @vertex.length < 2
-      @vertex.push ({ weight: input_signal, class_id: -1, density: 0, num_of_signals: 0, s: 0 })
+    if @graph.length < 2
+      @graph.add_vertex(weight: input_signal, class_id: -1, density: 0, num_of_signals: 0, s: 0)
       return true
     end
 
     winners = findWinners input_signal
     unless with_in_threshold? input_signal, winners[0], winners[1]
-      @vertex.push ({ weight: input_signal, class_id: -1, density: 0, num_of_signals: 0, s: 0 })
+      @graph.add_vertex(weight: input_signal, class_id: -1, density: 0, num_of_signals: 0, s: 0)
       return true
     end
 
     increment_edges_age winners[0]
     if add_edge? winners[0], winners[1]
+      @edges[winners[0]].merge!(winners[1] => 0)
+      @edges[winners[1]].merge!(winners[0] => 0)
+    else
+      @edges[winners[0]].delete winners[1]
+      @edges[winners[1]].delete winners[0]
     end
+
+    update_density(winners[0])
+    update_weights(winners[0], input_signal)
+    delete_old_edges
+
+    update_class_labels if iteration_count % @iter_threshold == 0
+    iteration_count += 1
+
+    true
   end
 
-  def increment_edges_age(vertex_num)
-    @edges[vertex_num].each_key do |key|
-      @edges[vertex_num][key] += 1
+  def increment_edges_age(vertex)
+    @graph.edges.each do |edge|
+      @age_edges[edge] += 1 if edge.source == vertex || edge.target == vertex
     end
   end
 
   def find_winners(input_signal)
     first_win_dist = 4_611_686_018_427_387_904
     second_win_dist = 4_611_686_018_427_387_904
-    first_win = -1
-    second_win = -1
-    @vertex.each_with_index do |vertex, index|
+    first_win = {}
+    second_win = {}
+    @graph.each_vertex do |vertex|
       dist = distance(input_signal, vertex[:weight])
 
       if dist < first_win_dist
         second_win = first_win
         second_win_dist = first_win_dist
-        first_win = index
+        first_win = vertex
         first_win_dist = dist
       elsif dist < second_win_dist
-        second_win = index
+        second_win = vertex
         second_win_dist = dist
       end
     end
@@ -70,36 +85,44 @@ class ESOINN
   def distance(first_point, second_point)
     i = 0
     sum = 0
-    sum += (first_point[i] - second_point[i])**2 while i < @dimensions
+    while i < @dimensions
+      sum += (first_point[i] - second_point[i])**2
+      i += 1
+    end
 
     Math.sqrt sum
   end
 
   def with_in_threshold?(input_signal, first_win, second_win)
-    return false if distance(input_signal, @vertex[first_win][:weight]) > get_similarity_threshold(first_win)
-    return false if distance(input_signal, @vertex[second_win][:weight]) > get_similarity_threshold(second_win)
+    return false if distance(input_signal, first_win[:weight]) > get_similarity_threshold(first_win)
+    return false if distance(input_signal, second_win[:weight]) > get_similarity_threshold(second_win)
 
     true
   end
 
-  def get_similarity_threshold(vertex_num)
+  def update_weights(winner, input_signal)
+    @vertex[winner][:weight].map!.with_index { |x, i| 1 / @vertex[winner][:num_of_signals] * (input_signal[i] - x) }
+    @edges[winner].keys.each do |neighbor|
+      @vertex[neighbor][:weight].map!.with_index { |x, i| 1 / 100 * @vertex[winner][:num_of_signals] * (input_signal[i] - x) }
+    end
+  end
+
+  def get_similarity_threshold(vertex)
     dist = 4_611_686_018_427_387_904
 
-    if @edges[vertex_num].length == 0 || @edges[vertex_num].nil?
-      @vertex.each_with_index do |vertex, index|
-        if index != vertex_num
-          dist_temp = distance(vertex[:weight], @vertex[vertex_num][:weight])
+    if @graph.out_degree(vertex) == 0
+      @graph.each_vertex do |v|
+        if v != vertex
+          dist_temp = distance(v[:weight], vertex[:weight])
           dist = dist_temp if dist < dist_temp
         end
       end
     else
       dist = -1
 
-      @edges[vertex_num].keys.each do |index|
-        if index != vertex_num
-          dist_temp = distance(@vertex[index][:weight], @vertex[vertex_num][:weight])
-          dist = dist_temp if dist > dist_temp
-        end
+      @graph.each_adjacent do |v|
+        dist_temp = distance(v[:weight], vertex[:weight])
+        dist = dist_temp if dist > dist_temp
       end
     end
 
@@ -107,11 +130,11 @@ class ESOINN
   end
 
   def add_edge?(first_winner, second_winner)
-    if @vertex[first_winner][:class_id] == -1 || @vertex[second_winner][:class_id] == -1
+    if first_winner[:class_id] == -1 || second_winner[:class_id] == -1
       true
-    elsif @vertex[first_winner][:class_id] == @vertex[second_winner][:class_id]
+    elsif first_winner[:class_id] == second_winner[:class_id]
       true
-    elsif @vertex[first_winner][:class_id] != @vertex[second_winner][:class_id] && merge_classes?(first_winner, second_winner)
+    elsif first_winner[:class_id] != second_winner[:class_id] && merge_classes?(first_winner, second_winner)
       true
     else
       false
@@ -119,13 +142,101 @@ class ESOINN
   end
 
   def merge_classes?(first_winner, second_winner)
-    class_id_a = @vertex[first_winner][:class_id]
+    class_id_a = first_winner[:class_id]
     mean_a = mean_density(class_id_a)
     max_a = max_density(class_id_a)
-    threshold_a = density_threshold(mean_density, max_density)
+    threshold_a = density_threshold(mean_a, max_a)
+
+    class_id_b = second_winner[:class_id]
+    mean_b = mean_density(class_id_b)
+    max_b = max_density(class_id_b)
+    threshold_b = density_threshold(mean_b, max_b)
+
+    min_ab = [first_winner[:density], second_winner[:density]].min
+    return true if min_ab > threshold_a * max_a && min_ab > threshold_b * max_b
+
+    false
   end
 
   def mean_density(class_id)
+    return 0 if class_id == -1
 
+    density = 0
+    cnt = 0
+    @graph.each_vertex do |vertex|
+      if vertex[:class_id] == class_id
+        density += vertex[:density]
+        cnt += 1
+      end
+    end
+
+    density / cnt
+  end
+
+  def max_density(class_id)
+    density = -4_611_686_018_427_387_904
+
+    @graph.each_vertex do |vertex|
+      if vertex[:class_id] == class_id && vertex[:density] > density
+        density = vertex[:density]
+      end
+    end
+
+    density
+  end
+
+  def density_thershold(mean_density, max_density)
+    if mean_density * 2 >= max_density
+      return 0
+    elsif mean_density * 3 >= max_density && max_density > mean_density * 2
+      return 0.5
+    else
+      return 1
+    end
+  end
+
+  def update_density(vertex)
+    m_distance = mean_distance vertex
+
+    vertex[:num_of_signals] += 1
+    vertex[:s] += 1 / ((1 + m_distance)**2)
+    vertex[:density] = vertex[:s] / vertex[:num_of_signals]
+  end
+
+  def delete_old_edges
+    @edges.each_value do |neighbors|
+      neighbors.each do |neighbor, age|
+        neighbors.delete neighbor if age > @max_age
+      end
+    end
+  end
+
+  def mean_distance(vertex)
+    m_distance = 0
+    cnt = 0
+    @graph.each_vertex do |v|
+      if v != vertex
+        m_distance += distance(vertex[:weight], v[:weight])
+        cnt += 1
+      end
+    end
+
+    m_distance / cnt
+  end
+
+  def update_class_labels
+    mark_classes
+    partition_classes
+    delete_noise_vertex
+  end
+
+  def mark_classes
+    @vertex.each do |vertex|
+      vertex[:class_id] = -1
+    end
+    class_id = 0
+    marked_edges = []
+    @edges.each do |_edge, _neighbors|
+    end
   end
 end
